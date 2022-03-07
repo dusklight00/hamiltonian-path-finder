b969736c661d3ae89f7241a8286861cbbc773a20
Irony00 <r.rahul.developer@gmail.com>
2022-03-07 12:59:54 +0530

    backend-api complete

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,50 @@
-from flask import Flask
+from flask import Flask, request, render_template, jsonify
+import numpy as np
+from engine.grid_graph_generator import GridGraphGenerator
+from engine.hamiltonian_path import HamiltonianPath
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+@app.route('/get_hamlin_path')
+def hamlin_path():
+    grid = [
+        [1, 1, 1],
+        [1, 1, 1]
+    ]
+    rows = np.array(grid).shape[0]
+    cols = np.array(grid).shape[1]
+    total_nodes = rows * cols
+
+    graph_generator = GridGraphGenerator(grid)
+    graph = graph_generator.get_graph()
+
+    hamiltonian_path = HamiltonianPath(graph)
+    hamlin_paths = hamiltonian_path.hamlin_path()
+    
+    # Sorting out complete hamlin paths
+    complete_hamlin_paths = []
+    for path in hamlin_paths:
+        if len(path) == total_nodes:
+            complete_hamlin_paths.append(path)
+    
+    # Sorting out max hamlin path
+    max_hamlin_path = []
+    for path in complete_hamlin_paths:
+        if len(path) > len(max_hamlin_path):
+            max_hamlin_path = path
+    
+    result = {
+        "complete_hamlin_paths": complete_hamlin_paths,
+        "max_hamlin_path": max_hamlin_path,
+        "hamlin_paths": hamlin_paths
+    }
+
+    return jsonify(result)
+
+
+    
+
+    
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
